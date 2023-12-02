56f4cb2d7fd88caccee50590654dc3dde4d69256
Dwe <alvarosanchezluque10@gmail.com>
2023-12-02 20:33:12 +0100

    bd

--- AW_TFG/settings.py
+++ AW_TFG/settings.py
@@ -101,7 +101,8 @@ DATABASES = {
 
 """
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    #"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.parse("postgres://db_django_r150_user:1hU2Tt6mf3H3ckYBrbtuee4MT1243gkL@dpg-cllkl07q7omc73cn7cng-a.oregon-postgres.render.com/db_django_r150")
 }
 
 # Password validation
