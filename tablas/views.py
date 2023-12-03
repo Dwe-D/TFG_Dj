f92fb2a49c58ef1d41989631f8e334cb6a79c399
Dwe <alvarosanchezluque10@gmail.com>
2023-12-03 20:50:37 +0100

    ttn-6

--- tablas/views.py
+++ tablas/views.py
@@ -4,19 +4,28 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from .models import NumerosModel
 # Create your views here.
-#@csrf_exempt
+@csrf_exempt
 def tabla(request):
 
     return render(request, "tablas/tabla.html")
 
 def procesar_datos_ttn(request):
-    data = request.body.decode("utf-8")
-    payload = json.loads(data)
-    numero1 = payload["data"]["uplink_message"]["decoded_payload"]["numero1"]
-    numero2 = payload["data"]["uplink_message"]["decoded_payload"]["numero2"]
+    try:
+        data = request.body.decode("utf-8")
+        payload = json.loads(data)
 
-    # Crear una instancia del modelo y guardar en la base de datos
-    numeros = NumerosModel(numero1=numero1, numero2=numero2)
-    numeros.save()
-    return HttpResponse()
-        
+        # Validate the decoded data
+        print(json.dumps(payload, indent=4))  # Print the decoded payload for inspection
+
+        numero1 = payload["data"]["uplink_message"]["decoded_payload"]["numero1"]
+        numero2 = payload["data"]["uplink_message"]["decoded_payload"]["numero2"]
+
+        # Create a new instance of the NumerosModel and save it to the database
+        numeros = NumerosModel(numero1=numero1, numero2=numero2)
+        numeros.save()
+
+        return HttpResponse()
+    except Exception as e:
+        # Handle any exceptions that occur during data processing
+        print(f"Error processing JSON data: {e}")
+        return HttpResponseBadRequest()
