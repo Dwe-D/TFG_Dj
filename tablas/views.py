41eb2a5e0cf7ce0c67e6c7f18c499fc02f016a5a
Dwe <alvarosanchezluque10@gmail.com>
2023-12-04 00:58:02 +0100

    ttn-30

--- tablas/views.py
+++ tablas/views.py
@@ -12,26 +12,20 @@ def tabla(request):
     return render(request, "tablas/tabla.html")
 
 @csrf_exempt
-
-@csrf_exempt
 def webhook(request):
     if request.method == 'POST':
         try:
             # Parsear el JSON de la solicitud
             data = json.loads(request.body)
 
-            # Imprimir el JSON recibido
-            #print("JSON recibido:", data)
-
             # Acceder a los valores de numero1 y numero2 dentro de decoded_payload
-            numero1 = data['data']['uplink_message']['decoded_payload']['numero1']
-            numero2 = data['data']['uplink_message']['decoded_payload']['numero2']
-            # Print the values
-            
-            numero1 = 12
-            numero2 = 13
-            print("Numero1:", numero1)
-            print("Numero2:", numero2)
+            numero1 = data.get('data', {}).get('uplink_message', {}).get('decoded_payload', {}).get('numero1')
+            numero2 = data.get('data', {}).get('uplink_message', {}).get('decoded_payload', {}).get('numero2')
+
+            # Verificar si los campos son None y establecer valores predeterminados
+            numero1 = 0 if numero1 is None else numero1
+            numero2 = 0 if numero2 is None else numero2
+
             # Guardar en la base de datos
             mi_modelo = NumerosModel(numero1=numero1, numero2=numero2)
             mi_modelo.save()
@@ -41,4 +35,5 @@ def webhook(request):
             return JsonResponse({'error': f'Error al procesar la solicitud: {str(e)}'}, status=500)
     else:
         return JsonResponse({'error': 'Solicitud no válida'}, status=400)
+
 
