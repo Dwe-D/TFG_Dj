0915e83184bf7ac5d050b8ce38618fa0bce33c36
Dwe <alvarosanchezluque10@gmail.com>
2023-12-04 00:08:22 +0100

    ttn-19

--- tablas/views.py
+++ tablas/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 import json
+import base64
 from .models import NumerosModel
 
 # Create your views here.
