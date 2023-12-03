283d65bc21f62a0d3d54a06daf3c51b550261de9
Dwe <alvarosanchezluque10@gmail.com>
2023-12-03 22:57:56 +0100

    ttn-12

--- tablas/urls.py
+++ tablas/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import tabla, procesar_datos_ttn
+from .views import tabla, webhook
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', tabla, name="tabla"),
-    path('deco', procesar_datos_ttn, name="deco" )
+    path('deco', webhook, name="deco" )
 
 ]
 
