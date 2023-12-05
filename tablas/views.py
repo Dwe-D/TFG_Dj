f6186ac734d5e902920373771ab02ea83442cd5e
Dwe <alvarosanchezluque10@gmail.com>
2023-12-05 19:17:20 +0100

    print

--- tablas/views.py
+++ tablas/views.py
@@ -17,14 +17,17 @@ def webhook(request):
         try:
             # Parsear el JSON de la solicitud
             data = json.loads(request.body)
- 
+            print("json: ", data)
             # Acceder a los valores de numero1 y numero2 dentro de decoded_payload
             numero1 = data.get('uplink_message', {}).get('decoded_payload', {}).get('numero1')
             numero2 = data.get('uplink_message', {}).get('decoded_payload', {}).get('numero2')
             numero3 = data.get('uplink_message', {}).get('decoded_payload', {}).get('numero3')
             bolea = data.get('uplink_message', {}).get('decoded_payload', {}).get('bolea')
             erbolean = True if bolea == 1 else False
-    
+            print("num1: ", numero1)
+            print("num2: ", numero2)
+            print("num3: ", numero3)
+            print("bolea: ", bolea)
 
             # Guardar en la base de datos
             mi_modelo = sensores(numero1=numero1, numero2=numero2, numero3=numero3, bolea=erbolean)
